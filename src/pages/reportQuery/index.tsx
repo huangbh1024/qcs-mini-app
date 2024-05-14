77e13e9b4874be63048fe6c27c5bc5f776bdb252
huangbh <578092215@qq.com>
2024-05-14 16:50:10 +0800

    style(page): 修改标题名

--- src/pages/reportQuery/index.tsx
+++ src/pages/reportQuery/index.tsx
@@ -14,7 +14,7 @@ export default () => {
         {Array.from({ length: 2 }).map((_, index) => (
           <ExhibitCard
             key={index}
-            title='序号'
+            title='报告单号'
             titleAction={<Text className='text-[#3dbedf]'>02308080283</Text>}
             bottomAction={
               <View className='w-full h-[36px] bg-[#3DBEDF] rounded-[4px] mt-[12px] flex justify-center items-center text-[16px] font-medium'>
