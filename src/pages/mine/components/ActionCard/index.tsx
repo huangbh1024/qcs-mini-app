f8da0dc2cf981b9f3505199346ed23464bf50971
huangbh <578092215@qq.com>
2024-05-15 16:38:09 +0800

    feat(page): 个人页列表路由跳转

--- src/pages/mine/components/ActionCard/index.tsx
+++ src/pages/mine/components/ActionCard/index.tsx
@@ -4,12 +4,13 @@ import smileIcon from '@/assets/icons/smile.png';
 import messageIcon from '@/assets/icons/message.png';
 import { View, Image, Text } from '@tarojs/components';
 import classNames from 'classnames';
+import { navigateTo } from '@tarojs/taro';
 
 export const ActionCard = () => {
   const actions = [
-    { text: '我的收藏', icon: startIcon },
-    { text: '满意度调查', icon: smileIcon },
-    { text: '意见反馈', icon: messageIcon },
+    { text: '我的收藏', icon: startIcon, page: 'favoritesList' },
+    { text: '满意度调查', icon: smileIcon, page: 'satisfactionSurvey' },
+    { text: '意见反馈', icon: messageIcon, page: 'feedback' },
   ];
   return (
     <View className='bg-white h-[368px] rounded-[8px] mt-[8px] flex-1 py-[10px] px-[8px]'>
@@ -21,6 +22,7 @@ export const ActionCard = () => {
               'flex-1 border-0 border-solid border-[#EBECEE] py-[16px] flex items-center justify-between',
               index !== actions.length - 1 && 'border-b-[1px]',
             ])}
+            onClick={() => navigateTo({ url: `/pages/${item.page}/index` })}
           >
             <Text className='text-[15px] text-[#09244B] font-[400]'>{item.text}</Text>
             <Image src={arrowRightIcon} className='w-[20px] h-[20px]' />
