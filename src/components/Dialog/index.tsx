899aa3abbe80e96abb00a3f7f3dd89345f234c0f
huangbh <578092215@qq.com>
2024-05-15 14:28:08 +0800

    style(component): 弹窗样式优化

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,5 +1,6 @@
 import { Dialog as NutDialog } from '@nutui/nutui-react-taro';
 import { Text, View } from '@tarojs/components';
+import classNames from 'classnames';
 import { ReactNode } from 'react';
 
 export interface Props {
@@ -7,23 +8,38 @@ export interface Props {
   content?: ReactNode;
   onCancel?: () => void;
   onConfirm?: () => void;
+  showCancelBtn?: boolean;
+  showTitle?: boolean;
 }
 export const Dialog = (props: Props) => {
-  const { visible, onCancel = () => {}, onConfirm = () => {}, content = '' } = props;
+  const {
+    visible,
+    onCancel = () => {},
+    onConfirm = () => {},
+    content = '',
+    showCancelBtn = true,
+    showTitle = true,
+  } = props;
+  const title = showTitle ? '提示' : '';
   return (
     <NutDialog
-      title='提示'
+      title={title}
       visible={visible}
       footer={
         <View className='w-full flex items-center justify-between text-[16px] font-[400]'>
+          {showCancelBtn && (
+            <View
+              className='h-[36px] bg-[#EFF1F3] rounded-[4px] w-[calc(50%-5px)] flex items-center justify-center'
+              onClick={onCancel}
+            >
+              <Text className='text-[#6B7C93]'>取消</Text>
+            </View>
+          )}
           <View
-            className='h-[36px] bg-[#EFF1F3] rounded-[4px] w-[calc(50%-5px)] flex items-center justify-center'
-            onClick={onCancel}
-          >
-            <Text className='text-[#6B7C93]'>取消</Text>
-          </View>
-          <View
-            className='h-[36px] bg-[#3DBEDF] rounded-[4px] w-[calc(50%-5px)] flex items-center justify-center'
+            className={classNames([
+              'h-[36px] bg-[#3DBEDF] rounded-[4px] flex items-center justify-center',
+              showCancelBtn ? 'w-[calc(50%-5px)]' : 'w-full',
+            ])}
             onClick={onConfirm}
           >
             <Text className='text-white'>确认</Text>
