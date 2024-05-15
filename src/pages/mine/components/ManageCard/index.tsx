7746f69317ea167286a557cf558b8f206fc19292
huangbh <578092215@qq.com>
2024-05-15 11:22:32 +0800

    feat(page): 个人页部分路由跳转

--- src/pages/mine/components/ManageCard/index.tsx
+++ src/pages/mine/components/ManageCard/index.tsx
@@ -2,20 +2,25 @@ import { View, Image, Text } from '@tarojs/components';
 import documentIcon from '@/assets/icons/document.png';
 import folderIcon from '@/assets/icons/folder.png';
 import healthyIcon from '@/assets/icons/healthy.png';
+import { navigateTo } from '@tarojs/taro';
 
 export const ManageCard = () => {
+  const routeTo = (page: 'appointmentRecord' | 'patientManagement' | 'medicalRecord') => {
+    const url = `/pages/${page}/index`;
+    navigateTo({ url });
+  };
   return (
     <View className='bg-white h-[107px] rounded-[8px] mt-[12px] flex'>
       <View className='w-[calc(100%/3)] h-full flex items-center justify-center flex-col'>
-        <Image src={folderIcon} className='w-[54px] h-[54px]' />
+        <Image src={folderIcon} className='w-[54px] h-[54px]' onClick={() => routeTo('patientManagement')} />
         <Text className='mt-[2px] text-[15px] text-[#09244B] font-[400]'>就诊人管理</Text>
       </View>
       <View className='w-[calc(100%/3)] h-full flex items-center justify-center flex-col'>
-        <Image src={documentIcon} className='w-[54px] h-[54px]' />
+        <Image src={documentIcon} className='w-[54px] h-[54px]' onClick={() => routeTo('appointmentRecord')} />
         <Text className='mt-[2px] text-[15px] text-[#09244B] font-[400]'>预约记录</Text>
       </View>
       <View className='w-[calc(100%/3)] h-full flex items-center justify-center flex-col'>
-        <Image src={healthyIcon} className='w-[54px] h-[54px]' />
+        <Image src={healthyIcon} className='w-[54px] h-[54px]' onClick={() => routeTo('medicalRecord')} />
         <Text className='mt-[2px] text-[15px] text-[#09244B] font-[400]'>就诊记录</Text>
       </View>
     </View>
